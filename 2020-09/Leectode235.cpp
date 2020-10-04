66293a8b1265bbd3869fc7aec40d6fdd70a1ba31
zznEo <954228113@qq.com>
2020-10-04 18:06:52 +0800

    2020/09/27

---
+++ 2020-09/Leectode235.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    vector<TreeNode*> getPath(TreeNode* root, TreeNode* target) {
+        vector<TreeNode*> path;
+        TreeNode* node = root;
+        while (node != target) {
+            path.push_back(node);
+            if (target->val < node->val) {
+                node = node->left;
+            }
+            else {
+                node = node->right;
+            }
+        }
+        path.push_back(node);
+        return path;
+    }
+
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        vector<TreeNode*> path_p = getPath(root, p);
+        vector<TreeNode*> path_q = getPath(root, q);
+        TreeNode* ancestor;
+        for (int i = 0; i < path_p.size() && i < path_q.size(); ++i) {
+            if (path_p[i] == path_q[i]) {
+                ancestor = path_p[i];
+            }
+            else {
+                break;
+            }
+        }
+        return ancestor;
+    }
+};
+
+Solution s1;
+int main() {
+    
+    system("pause");
+    return 0;
+}
